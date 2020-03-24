66c49535b490e0be2e5701b68926cb5d7d6c1c87
romadmn <ferencrman@gmail.com>
2020-03-25 00:02:34 +0200

    Closes #18 Create single page for book

--- src/app/shared/components/book/book.component.ts
+++ src/app/shared/components/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { RequestService } from 'src/app/core/services/request/request.service';
+import { BooksService } from 'src/app/core/services/books.service';
 import { ActivatedRoute } from '@angular/router';
 import { bookUrl } from 'src/app/configs/api-endpoint.constants';
 import { book } from "src/app/core/models/book";
@@ -10,7 +11,7 @@ import { book } from "src/app/core/models/book";
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
-  providers: [RequestService]
+  providers: [RequestService, BooksService]
 })
 export class BookComponent implements OnInit {
 
@@ -20,7 +21,9 @@ export class BookComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private bookService:BooksService,
+    private requestService:RequestService
     ) {}
   
   ngOnInit() {
@@ -29,14 +32,11 @@ export class BookComponent implements OnInit {
       switchMap(params => params.getAll('id'))
   )
   .subscribe(data=> this.bookId = +data);
-  this.getBookById(this.bookId);
 
+  this.bookService.getBookById(this.bookId).subscribe((value: book) => {
+    this.book = value;
+  });
   }
   
-  getBookById(bookId: number) {
-    return this.http.get<book>(this.baseUrl + `/${bookId}`).subscribe((value: book) => {
-      this.book = value;
-    });
-  }
 
 }
