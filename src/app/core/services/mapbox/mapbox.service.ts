a099ae4cead50f9b985a416e4a715143385c438b
Maryna Diuh <maradug13@gmail.com>
2020-04-07 17:42:25 +0300

    added geocoder for  search

--- src/app/core/services/mapbox/mapbox.service.ts
+++ src/app/core/services/mapbox/mapbox.service.ts
@@ -1,30 +1,46 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import * as mapboxgl from 'mapbox-gl';
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import * as mapboxgl from "mapbox-gl";
+import * as MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class MapboxService {
+  map: mapboxgl.Map;
+  style = "mapbox://styles/mapbox/streets-v11";
+  lat = 49.83305;
+  lng = 23.997775;
+  zoom = 16;
 
-map: mapboxgl.Map;
-style = 'mapbox://styles/mapbox/streets-v11';
-lat = 49.833050;
-lng = 23.997775;
-zoom = 16;
+  geocoder;
 
   constructor() {
     mapboxgl.accessToken = environment.mapbox.accessToken;
-   }
+  }
 
-   buildMap() {
+  buildMap() {
     this.map = new mapboxgl.Map({
-      container: 'map',
+      container: "map",
       style: this.style,
       zoom: this.zoom,
-      center: [this.lng, this.lat]
-    })
-    this.map.addControl(new mapboxgl.NavigationControl());
+      center: [this.lng, this.lat],
+    });
+
+    const marker = new mapboxgl.Marker()
+      .setLngLat([this.lng, this.lat])
+      .addTo(this.map);
+
+    this.geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      placeholder: 'Search',
+      mapboxgl: mapboxgl,
+      marker: false
+    });
+
+    // this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.addControl(this.geocoder);
+    // this.map.addControl(new mapboxgl.MapboxGeocoder());
   }
 
   //  getMarkers(){
