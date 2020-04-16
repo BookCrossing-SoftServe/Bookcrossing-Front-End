6fdd4b822a990c9abeb8724575827d33932b71c5
Maryna Diuh <maradug13@gmail.com>
2020-04-16 13:35:16 +0300

    genre service

--- src/app/shared/components/add-book/add-book.component.ts
+++ src/app/shared/components/add-book/add-book.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IBook } from 'src/app/core/models/book';
 import { IAuthor } from 'src/app/core/models/author';
 import { BookService } from 'src/app/core/services/book/book.service';
+import { GenreService } from 'src/app/core/services/genre/genre';
 
 @Component({
   selector: 'app-add-book',
@@ -11,23 +12,23 @@ import { BookService } from 'src/app/core/services/book/book.service';
   styleUrls: ['./add-book.component.scss'],
 })
 export class AddBookComponent implements OnInit {
-  constructor(private bookService: BookService) {}
+  constructor(
+    private bookService: BookService,
+    private genreService: GenreService
+  ) {}
   addBookForm: FormGroup;
   authorControl: FormGroup;
   addNewAuthor: boolean = false;
 
   userId: number = 1;
 
-  genres: IGenre[] = [
-    { id: 1, name: 'Fantazy' },
-    { id: 2, name: 'Horror' },
-    { id: 3, name: 'Science fiction' },
-  ];
+  genres: IGenre[] = [];
 
   authors: IAuthor[] = [];
 
   ngOnInit(): void {
     this.buildForm();
+    this.getAllGenres();
   }
 
   buildForm() {
@@ -40,7 +41,7 @@ export class AddBookComponent implements OnInit {
     this.addBookForm = new FormGroup({
       title: new FormControl(null, Validators.required),
       genres: new FormControl(null, Validators.required),
-      publisher: new FormControl(null)
+      publisher: new FormControl(null),
     });
   }
 
@@ -84,6 +85,17 @@ export class AddBookComponent implements OnInit {
 
   getGenreById(id: number) {
     return this.genres ? this.genres.find((genre) => genre.id == id)?.name : '';
+  }
+
+  getAllGenres() {
+    this.genreService.getGenre().subscribe(
+      (data) => {
+        this.genres = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   onDeleteAuthor(author: IAuthor) {
