8e772bd2cf2894a15a44165b7f8297af36e80ddc
Maksym <mm.novitskyi@gmail.com>
2020-05-11 20:24:52 +0300

    Static dashboard ui

--- src/app/shared/components/admin/dashboard/dashboard.component.ts
+++ src/app/shared/components/admin/dashboard/dashboard.component.ts
@@ -24,20 +24,80 @@ export class DashboardComponent implements OnInit {
 
   public chartType = 'line';
   public chartDatasets: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 35, 45], label: 'Amount of Books Received' },
+    { data: [19, 15, 27, 28, 25, 31, 42], label: 'Books Registered' },
+    { data: [19, 12, 17, 25, 24, 28, 32], label: 'New Users' },
   ];
   public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public chartColors: Array<any> = [
     {
-      backgroundColor: 'rgba(0, 137, 132, .2)',
-      borderColor: 'rgba(0, 10, 130, .7)',
+      backgroundColor: 'rgba(255, 255, 255, .2)',
+      borderColor: 'rgba(242, 242, 242, .7)',
+      borderWidth: 2,
+    },
+    {
+      backgroundColor: 'rgba(255, 153, 153, .2)',
+      borderColor: 'rgba(191, 191, 191, .7)',
       borderWidth: 2,
     }
   ];
   public receivedChartOptions: any = {
     responsive: true,
-    steppedLine: true
+    scales: {
+      xAxes: [{
+        gridLines: {
+          color: 'rgba(166, 166, 166, 0.2)',
+        },
+        ticks: {
+          fontColor: 'white',
+        },
+      }],
+      yAxes: [{
+        gridLines: {
+          color: 'rgba(166, 166, 166, 0.2)',
+        },
+        ticks: {
+          fontColor: 'white',
+        },
+      }]
+    },
+    legend: {
+      labels: {
+        fontColor: 'white',
+      },
+    }
   };
+
+
+  public barChartType = 'bar';
+  public barChartDatasets: Array<any> = [
+    { data: [30, 19, 22, 17, 13, 18, 20, 18], label: 'Registered Books' }
+  ];
+
+  public barChartLabels: Array<any> = ['Lviv', 'Dnipro', 'Kyiv', 'Chernivtsi', 'Kharkiv', 'Rivne', 'Ivano-Frankivsk'];
+
+  public barChartColors: Array<any> = [
+    {
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(40, 255, 20, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(40, 255, 20, 1)'
+      ],
+      borderWidth: 2,
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
