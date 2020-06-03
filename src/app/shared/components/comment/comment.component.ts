071698bfbe4bf0f615aea788f565a82f09d445fb
romadmn <ferencrman@gmail.com>
2020-06-03 18:14:30 +0300

    Fix

--- src/app/shared/components/comment/comment.component.ts
+++ src/app/shared/components/comment/comment.component.ts
@@ -1,56 +1,70 @@
-import {Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {CommentService} from 'src/app/core/services/commment/comment.service';
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
-import {IUser} from '../../../../core/models/user';
-import {CommentService} from '../../../../core/services/commment/comment.service';
-import {IChildDeleteComment} from '../../../../core/models/comments/child-comment/childDelete';
-import {IChildUpdateComment} from '../../../../core/models/comments/child-comment/childUpdate';
-import {IChildInsertComment} from '../../../../core/models/comments/child-comment/childInsert';
+import {IUser} from '../../../core/models/user';
+import {__await} from 'tslib';
+import {AuthenticationService} from '../../../core/services/authentication/authentication.service';
+import {UserService} from '../../../core/services/user/user.service';
+import {IRootComment} from '../../../core/models/comments/root-comment/root';
+import {IRootInsertComment} from '../../../core/models/comments/root-comment/rootInsert';
+import {IRootDeleteComment} from '../../../core/models/comments/root-comment/rootDelete';
+import {IRootUpdateComment} from '../../../core/models/comments/root-comment/rootUpdate';
+import {element} from 'protractor';
 
 @Component({
-  selector: 'app-childcomment',
-  templateUrl: './childcomment.component.html',
-  styleUrls: ['./childcomment.component.scss']
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.scss'],
+
 })
-export class ChildcommentComponent implements OnInit {
-  @Input() comments;
-  @Input() level;
-  @Input() user: IUser;
-  @Input() ids;
-  @Output() update = new EventEmitter();
-  @Input() isAuthorized;
+export class CommentComponent implements OnInit {
+  @Input() bookId = 0;
+  comments: IRootComment[];
+  user: IUser;
   text = '';
+  rating = 0;
+  level = 0;
 
 
-  UpdateComments() {
-    this.update.next();
+  constructor(private  commentservice: CommentService, private authenticationService: AuthenticationService,
+              private userService: UserService
+  ) {
   }
+
   increment() {
     return this.level++;
   }
 
-  constructor(private  commentservice: CommentService) {
+  ngOnInit() {
+    this.updateComments();
+    this.getUser()
   }
 
-  ngOnInit(): void {
-
+  isAuthenticated(){
+    return this.authenticationService.isAuthenticated()
   }
 
-  formatDate(date) {
-
-    TimeAgo.addLocale(en);
-    const d = new Date(date);
-    const timeAgo = new TimeAgo('en-US');
-    return timeAgo.format(d);
-  }
-
-  CanEditCommnet(owner) {
-    if (owner === null || this.user === null) {
-      return false;
-    } else {
-      return owner.id === this.user.id;
+  getUser(){
+    if(this.isAuthenticated()){
+      console.log("Authencicated")
+      this.authenticationService.getUserId().subscribe((value: number)=> {
+        this.userService.getUserById(value).subscribe((value: IUser)=>{
+          this.user = value;
+        })
+      })
     }
   }
+
+
+  canCommit() {
+    return this.isAuthenticated() && (this.text !== '');
+  }
+
+  CopyText(text) {
+    return text;
+  }
+
 
   getUserName(owner) {
     if (owner === null) {
@@ -65,44 +79,67 @@ export class ChildcommentComponent implements OnInit {
 
     }
   }
-  canCommit() {
-    return this.isAuthorized && (this.text !== '');
+
+  CanEditCommnet(owner) {
+    if (owner === null || typeof this.user === 'undefined') {
+      return false;
+    } else {
+      return owner.id === this.user.id;
+    }
   }
+
+  formatDate(date) {
+
+    TimeAgo.addLocale(en);
+    const d = new Date(date);
+    const timeAgo = new TimeAgo('en-US');
+    return timeAgo.format(d);
+  }
+
   returnID(id) {
-    let newids = this.ids.slice();
-    newids.push(id);
-    return newids;
+    let ids = [];
+    ids.push(id);
+    return ids;
   }
 
-  async deleateComment(id) {
-    let newids = this.returnID(id);
-    let deleteComment: IChildDeleteComment = {
-      id: newids, ownerId: this.user.id
-    }
-    this.commentservice.deleteChildComment(deleteComment).subscribe((r) => {
+  updateComments() {
+    this.commentservice.getComments(this.bookId).subscribe((value: IRootComment[])=> {
+      this.comments = value;
     });
-    this.UpdateComments();
   }
 
-  updateComment(id, text) {
-    let newids = this.returnID(id);
-    let updateComment: IChildUpdateComment = {
-      id: newids, ownerId: this.user.id, text: text
+  async PostComment() {
+    let postComment: IRootInsertComment = {
+      bookId: this.bookId, ownerId: this.user.id, rating: this.rating, text: this.text
     }
-    this.commentservice.updateChildComment(updateComment).subscribe((r) => {
-    });
-    this.UpdateComments();
-  }
-
-  PostComment() {
-    let postComment: IChildInsertComment = {
-      id: this.ids, ownerId: this.user.id, string: this.text
-
-    }
-    this.commentservice.postChildComment(postComment).subscribe((r) => {
+    this.commentservice.postComment(postComment).subscribe((r) => {
 
     });
     this.text = '';
-    this.UpdateComments();
+    this.ngOnInit()
   }
+
+  async deleateComment(id) {
+    let deleteComment: IRootDeleteComment = {
+      id: id, ownerId: this.user.id
+
+    }
+    this.commentservice.deleteComment(deleteComment).subscribe((r) => {
+    });
+    this.ngOnInit()
+  }
+
+  async updateComment(id, text) {
+    let updateComment: IRootUpdateComment = {
+      id: id, ownerId: this.user.id, rating: this.rating, text: text
+    }
+    this.commentservice.updateComment(updateComment).subscribe((r) => {
+    });
+    this.ngOnInit()
+  }
+
+  onRatingSet($event: number) {
+    this.rating = $event;
+  }
+
 }
